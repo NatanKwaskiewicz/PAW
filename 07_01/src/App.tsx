d655666968651bc4f78f7e252c2d52a2011353f6
Natan <natan.kwaskiewicz@gmail.com>
2026-01-13 20:20:11 +0100

    add NavBar component and styling

--- 07_01/src/App.tsx
+++ 07_01/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router'
 import Post from './components/Post/Post.tsx'
 import Home from './components/Home/Home.tsx'
 import CategoryList from "./components/CategoryList/CategoryList.tsx";
+import NavBar from "./components/NavBar/NavBar.tsx";
 import './App.css'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     return (
         <>
             <BrowserRouter>
+                <NavBar />
                 <Routes>
                     <Route index element={<Home />} />
                     <Route path="post" element={<Post />} />
--- 07_01/src/components/NavBar/NavBar.tsx
+++ 07_01/src/components/NavBar/NavBar.tsx
@@ -4,9 +4,12 @@ import styles from './NavBar.module.scss'
 const NavBar = () => {
     return (
         <div className={styles.NavBar}>
+            <h1>Blog</h1>
+            <section>
             <Link to='/'>Home</Link>
             <Link to='/post'>Post</Link>
             <Link to='/categories'>Categories</Link>
+            </section>
         </div>
     );
 }
