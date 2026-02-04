a3e11ba2be902a563d59292b6b4092b2313f94f3
Natan Kwaśkiewicz <118686262+NatanKwaskiewicz@users.noreply.github.com>
2026-02-04 15:37:47 +0100

    Add files via upload

---
+++ 04_02/src/components/Post/Post.tsx
@@ -0,0 +1,82 @@
+import styles from './Post.module.scss'
+import {useState, useEffect} from "react";
+import {useParams} from "react-router";
+import type {Post} from '../../types/Post/Post.ts'
+import type {User} from '../../types/User/User.ts'
+
+const Post = () => {
+    const [post, setPost] = useState<Post>()
+    const [isPostLoading, setIsPostLoading] = useState(false)
+    const [isUserLoading, setIsUserLoading] = useState(false)
+    const [isError, setIsError] = useState(false)
+    const [user, setUser] = useState<User>()
+    const { id }  = useParams()
+
+    useEffect(() => {
+        (()=> {
+            setIsPostLoading(true)
+        })()
+        fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+            .then(response => response.json())
+            .then((json: Post) => {
+                setPost(json)
+            })
+            .catch(() => {
+                setIsError(true)
+            })
+            .finally(() => {
+                setIsPostLoading(false)
+            })
+    }, [])
+
+    console.log(post)
+    useEffect(() => {
+        if (!post?.userId) return;
+
+        (()=> {
+            setIsUserLoading(true)
+        })()
+        fetch(`https://jsonplaceholder.typicode.com/users/${post.userId}`)
+            .then(response => response.json())
+            .then((json: User) => {
+                setUser(json)
+            })
+            .catch(()=> {
+                setIsError(true)
+            })
+            .finally(() => {
+                setIsUserLoading(false)
+            })
+    }, [post?.userId])
+
+
+    return (
+        <div className={styles.Post}>
+            {isPostLoading || isUserLoading && (
+                <div className={styles.PostListLoading}>
+                    Loading...
+                </div>
+            )}
+            {isError && (
+                <div className={styles.PostListError}>
+                    An error occurred while fetching posts.
+                </div>
+            )}
+            {!isPostLoading && !isUserLoading && !isError && (
+                <>
+                <h3 className={styles.PostTitle}>
+                    {post?.title}
+                </h3>
+                <p className={styles.PostBody}>
+                    {post?.body}
+                </p>
+                    <h5 className={styles.PostUser}>
+                        User: {user?.username}
+                    </h5>
+                </>
+            )}
+        </div>
+    );
+}
+
+export default Post;
