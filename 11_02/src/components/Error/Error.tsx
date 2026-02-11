20558a760e5086a9f4bec70d3741dacbe7f62f73
Natan Kwaśkiewicz <118686262+NatanKwaskiewicz@users.noreply.github.com>
2026-02-11 12:09:05 +0100

    React query

---
+++ 11_02/src/components/Error/Error.tsx
@@ -0,0 +1,15 @@
+import styles from './Error.module.scss'
+
+interface ErrorProps {
+    message: string;
+}
+
+const Error = ({message} : ErrorProps) => {
+    return (
+        <div className={styles.Error}>
+            {message}
+        </div>
+    )
+}
+
+export default Error
