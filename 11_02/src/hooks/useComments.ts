6db92e4ce0e8591313d6f1b035478b2ea8452d58
Natan <natan.kwaskiewicz@gmail.com>
2026-02-15 13:24:00 +0100

    fix queryParam for useComments

--- 11_02/src/hooks/useComments.ts
+++ 11_02/src/hooks/useComments.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query"
 import type {Comment} from '../types/Comment/Comment.ts'
 
 export const getComments = async (id: string | undefined) => {
-    return await fetch(`https://jsonplaceholder.typicode.com/comments?${id}`).then(res => res.json())
+    return await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${id}`).then(res => res.json())
 }
 
 export const useComments = (id: string | undefined) => {
--- 11_02/src/main.tsx
+++ 11_02/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.scss'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+//import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 import {BrowserRouter} from "react-router";
 
 const queryClient = new QueryClient();
@@ -14,7 +14,7 @@ createRoot(document.getElementById('root')!).render(
             <BrowserRouter>
                 <App />
             </BrowserRouter>
-            <ReactQueryDevtools />
+            {/*<ReactQueryDevtools buttonPosition={"bottom-left"}/>*/}
         </QueryClientProvider>
     </StrictMode>,
 )
